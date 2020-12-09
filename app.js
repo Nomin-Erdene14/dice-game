638dca504c69312782524ced150dd86d44cda488
Nomin <nominb014@gmail.com>
2020-12-09 16:26:06 +0800

    toglogchiin eeljiig solih

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Toglogchiin eeljiig hadgalh huwisagch, 1r toglogchiig 0, 2r toglogchiig 2 gj temdegley.
-var activePlayer = 1; 
+var activePlayer = 0; 
 
 
 
@@ -22,13 +22,44 @@ window.document.getElementsByName("current-1").textContent ="0";
 
 var diceDom = window.document.querySelector(".dice");
 diceDom.style.display = "none"
-
+// shoog shideh event
 window.document.querySelector(".btn-roll").addEventListener('click',function (){
+    //1-6 sanamsargui neg too gargna
     var diceNumber = Math.floor(Math.random() * 6)+1;
- 
-
-
+    //Shoonii zurgiig web deer gargaj irne
    diceDom.style.display = "block";
+   //buusan sanamsargui toond hargalzah zurgiig web dr gargaj irne
    diceDom.src = "dice-" + diceNumber + ".png";
+   //buusan too ni 1s ylgaatai bol idewhtei toglogchiin onoog nemne
+   if(diceNumber !==1){
+       //1s ylgaatai too buulaa.Buusan toog toglogchid nemj ogno.
+       roundScore = roundScore+diceNumber;
+       window.document.getElementById('current-'+ activePlayer).textContent=roundScore;
+   }else{
+       //1 buusan tul toglogchiin eeljiig neg heseg solij bolno
+       //ene toglogchiin  eeljin dr cugluulsn onoog noil bolgno
+       roundScore =0;
+       window.document.getElementById('current-'+ activePlayer).textContent=0;
+       //toglogchiin eelijiig nogoo toglogch ruu shiljuulne
+       activePlayer===0 ? (activePlayer=1) : (activePlayer=0);
+       //ulaan tsegiig shiljuulne
+       window.document.querySelector(".player-0-panel").classList.toggle("active");
+       window.document.querySelector(".player-1-panel").classList.toggle("active");
+       
+      //shoog alga bolgoh
+      diceDom.style.display = "none";
+    
+
+    
+       // herew idwhtei toglogch ni  0 baiwal idewhtei toglogchiig 1 bolno.
+       
+       // vgvi bol  idwhtei toglogch   0 bolgono.
+
+    
+      
+
+   }
+
+    
 });
 
